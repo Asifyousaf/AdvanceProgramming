0cc51336e1c58df0a43a4f6caf3b97fcebe66028
Asif Yousaf <Asifyousaf14@gmail.com>
2023-12-12 05:26:34 -0800

    Commit

--- Assessment 1/Chapter 1 - Python Refresher/Bonus & Further Exercises/Exercise V - Lamda function.py
+++
@@ -1,6 +0,0 @@
-marks = [("CodeLab I", 67), ("web Development", 75), ("CodeLabII", 74), ("Smartphone Apps", 68), ("Games Development", 70), ("Responsive web", 65)]
-
-# Sorting without specifying a key (default behavior)
-sorted_result = sorted(marks, key=lambda x: x)
-
-print("Sorted without specifying a key:", sorted_result)
